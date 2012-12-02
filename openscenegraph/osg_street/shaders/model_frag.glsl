0934d52d43d53e6d43d5abe54f672cbc9aea228b
Preet Desai <preet.desai@gmail.com>
2012-12-02 15:26:35 -0500

    * openscenegraph/osg_street: playing around with shaders and polyline offset fixes

--- openscenegraph/osg_street/shaders/model_frag.glsl
+++ openscenegraph/osg_street/shaders/model_frag.glsl
@@ -5,11 +5,14 @@ varying vec2 TexCoord1;
 
 void main()
 {
-    float cVal = fract(TexCoord1.y*80.0); //sin(TexCoord1.y*16.0);
-    if(cVal > 0.5)
-    {   cVal = 1.0;   }
-    else
-    {   cVal = 0.0;   }
+//    float cVal = fract(TexCoord1.y*80.0); //sin(TexCoord1.y*16.0);
+//    if(cVal > 0.5)
+//    {   cVal = 1.0;   }
+//    else
+//    {   cVal = 0.0;   }
 
-    gl_FragColor = vec4(cVal,cVal,cVal,1.0);
+//    gl_FragColor = vec4(cVal,cVal,cVal,1.0);
+
+
+    gl_FragColor = vec4(TexCoord1.y,1.0,1.0-TexCoord1.y,1.0);
 }
